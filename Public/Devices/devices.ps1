df5aaefbd5247a8bccfa932cd762f1e49caea816
Aaron <aglenn@laureateinstitute.org>
2022-01-24 17:53:35 -0600

    flesh out the devices area

--- Public/Devices/devices.ps1
+++ Public/Devices/devices.ps1
@@ -1,4 +1,11 @@
-function Get-LNMSDevices {}
-
+function Add-LNMSDevice{}
 function Get-LNMSDevice {}
 
+function Remove-LNMSDevice {}
+
+function Start-LNMSDeviceDiscovery {}
+
+function Add-LNMSDeviceParents {}
+function Remove-LNMSDeviceParents {}
+
+function Get-LNMSDeviceParents {}
