8774b8832754d77a1b9fa8bffdfc059b05ff0ed6
Aaron Glenn <aaron@aaroneg.com>
2022-12-21 16:30:40 -0600

    implement system endpoint

---
+++ Public/endpoints/system.ps1
@@ -0,0 +1,16 @@
+$LNMSSystemPath="system"
+Function Get-LNMSSystem {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory = $false)]
+		[Object]$Connection=$Script:LNMSConnection
+	)
+
+	$restParams= @{
+		Method = 'Get'
+		URI = "$($Connection.ApiBaseURL)/$LNMSSystemPath"
+		body = $PostJson
+	}
+	$PostObject = Invoke-CustomRequest -restParams $restParams -Connection $Connection
+	$PostObject.System
+}
