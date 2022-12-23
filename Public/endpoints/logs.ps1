530d50f977e275746c0ea0cf51610cd8fb74d861
Aaron Glenn <aaron@aaroneg.com>
2022-12-23 12:57:52 -0600

    support note added for logs

--- Public/endpoints/logs.ps1
+++ Public/endpoints/logs.ps1
@@ -1,4 +1,6 @@
 # This endpoint is only partially implemented
+# The documents say that the logtype can also be "auth", but in testing against 
+# version 22.11.0-36-g098bbc6bb, this API route did not exist.
 
 Function Get-LNMSDeviceLog {
 	[CmdletBinding()]
